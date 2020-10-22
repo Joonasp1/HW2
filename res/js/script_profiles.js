d18a7a49b4b7575e17fcf3bcbdf945ffa67eb2c1
username12355 <kristjan.korela@gmail.com>
2020-10-22 22:41:40 +0300

    Task 3. Added browse.html for profiles, made a script that adds all the profiles into the page with proper layout, each square displays the profile picture and full name of each person.

---
+++ res/js/script_profiles.js
@@ -0,0 +1,40 @@
+$(function () {
+    loadProfiles()
+        .then(function (profilesResponse) {
+            console.log(profilesResponse);
+            profiles = profilesResponse;
+            displayProfiles(profiles);
+        })
+        .catch(function () {
+            alert('Error loading profiles info')
+        });
+});
+
+function loadProfiles() {
+    return $.get(
+        {
+            url: 'https://private-anon-68a469624e-wad20postit.apiary-mock.com/profiles',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    );
+}
+
+function displayProfiles(profiles) {
+    for (let i = 0; i < profiles.length; i++) {
+        let addBody = (`
+        <div class="profile">
+            <div class="profile-cropper">
+                <img src=${profiles[i].avatar}>
+            </div>
+            <b>${profiles[i].firstname} ${profiles[i].lastname}</b>
+        </div>
+        `);
+
+        $('.main-container').append(addBody)
+    }
+}
